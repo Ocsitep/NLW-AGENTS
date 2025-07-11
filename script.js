8f729375742489b27e5d0cec21d769b4b0bf11b1
Ocsitep <rmoraisfotos@gmail.com>
2025-07-11 12:13:58 -0300

    App done. Missing some ajustments in the future

--- script.js
+++ script.js
@@ -5,16 +5,42 @@ const askButton = document.getElementById("askButton");
 const form = document.getElementById("form");
 const aiResponse = document.getElementById("aiResponse");
 
-
+const markdownToHTML = (text) => {
+  const converter = new showdown.Converter();
+  return converter.makeHtml(text);
+};
 
 const askAI = async (question, game, apiKey) => {
   const model = "gemini-2.5-flash";
   const geminiURL = `https://generativelanguage.googleapis.com/v1beta/models/${model}:generateContent?key=${apiKey}`;
 
-  const userQuestion = `Sendo um especialista no game ${game}, me responda a pergunta: ${question}. Quero uma resposta objetiva, mas completa com explicações.`;
+  const userQuestion = `
+    ## Especialidade
+    Você é um especialista assistente de meta para o jogo ${game}.
+    ## Tarefa
+    Você deve responder a pergunta do usuário com base no seu conhecimento sobre o jogo.
+    Estratégias, builds, dicas e truques.
+    ## Regras
+     - Se você não sabe a resposta, diga que não sabe. Não tente inventar uma resposta.
+     - Se a pergunta não for relacionada ao jogo, responda com "Essa pergunta não é sobre o jogo".
+      - Considere a data atual ${new Date().toLocaleDateString()}.
+     - Faça pesquisas atualizadas utilizando o patch atual do jogo para responder. Baseada na data atual.
+     - Nunca responda itens que você não tenha certeza de que exista no patch atual do jogo.
+    ## Resposta 
+    -Economize na resposta. Seja direto e objetivo. 
+    -No máximo 600 caracteres
+    - Não precisa de nenhuma, saudação, despedida ou agradecimento.
+    - Responda apenas a pergunta do usuário.
+    - Organize a resposta em tópicos, se necessário.
+    - Use markdown para formatar a resposta.
+    - Use emojis gamers para deixar a resposta mais divertida.
+    ## Pergunta do usuário
+    ${question}?
+  `;
 
   const contents = [
     {
+      role: "user",
       parts: [
         {
           text: userQuestion,
@@ -22,6 +48,13 @@ const askAI = async (question, game, apiKey) => {
       ],
     },
   ];
+
+  const tools = [
+    {
+      google_search: {},
+    },
+  ];
+
   //chamada API
   const response = await fetch(geminiURL, {
     method: "POST",
@@ -30,12 +63,12 @@ const askAI = async (question, game, apiKey) => {
     },
     body: JSON.stringify({
       contents,
+      tools,
     }),
   });
 
   const data = await response.json();
-  console.log({ data });
-  return data.candidates[0].content.parts[0].text
+  return data.candidates[0].content.parts[0].text;
 };
 
 const sendForm = async (event) => {
@@ -54,6 +87,9 @@ const sendForm = async (event) => {
 
   try {
     const text = await askAI(question, game, apiKey);
+    aiResponse.querySelector(".response-content").innerHTML =
+      markdownToHTML(text);
+    aiResponse.classList.remove("hidden");
   } catch (error) {
     console.log("Erro ao enviar o formulário:", error);
     alert("Ocorreu um erro ao enviar a pergunta. Por favor, tente novamente.");
